41607315e343791a8186652e284afb13de5b7c8a
melsil11 <112329390+melsil11@users.noreply.github.com>
2023-01-12 11:43:01 -0500

    completed exercises

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,31 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
-
+function palindrome(str) {   
+    // i is for index 
+    // not ideal solution but gives you something to talk about with the interviewer
+    return str.split('').every(( char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
 module.exports = palindrome;
+
+
+// easy solution 
+// function palindrome(str) {   
+//     const reversed = str.split('').reverse().join('');
+
+//     return str === reversed;
+// }
+// module.exports = palindrome;
+
+// clean code version of above code
+// function palindrome(str) {   
+//     const reversed = str
+//  .split('')
+//  .reverse()
+//  .join('');
+
+// return str === reversed;
+// }
+// module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,44 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((reversed, character) => character + reversed, '');
+}
 
 module.exports = reverse;
+
+// ////easiest solution but interview might ask for an alternative way of solving this
+// function reverse(str) {
+//     // /////not very clean code
+//     // const arr = str.split('')
+//     // arr.reverse()
+//     // return arr.join('')
+
+//     // clean code version 
+//     return str
+//     .split('')
+//     .reverse()
+//     .join('')
+// }
+
+// //// for loop solution with new for loop syntax instead of the let i = 0; i < 0; i++
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+    
+//     return reversed;
+// }
+
+
+// using debugger
+// function reverse(str) {
+//     debugger;
+//     return str.split('').reduce((reversed, character) => character + reversed, '');
+// }
+// reverse('apple');
+// module.exports = reverse;
+
+// make sure to remove the debugger when done and if need be remove the called function if it wasn't necessary 
